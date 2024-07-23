afaa95bc8265708e68f32134f404d04bedc0f652
TajulMunandar <tajulmunandar701@gmail.com>
2024-07-23 18:18:04 +0700

    api

---
+++ app/Http/Controllers/API/AuthMobileController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\DataPasien;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Sanctum\HasApiTokens;
+
+class AuthMobileController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $data = DataPasien::where('user_id', $user->id)->first();
+
+            $token = $user->createToken('authToken')->plainTextToken;
+
+            return response()->json(['access_token' => $token, 'user_profile' => $user, 'data' => $data], 200);
+        }
+
+        return response()->json(['message' => 'Unauthorized'], 401);
+    }
+
+    public function register(Request $request)
+    {
+        // Validate request inputs
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'username' => 'required|string|unique:users,username|max:255',
+            'password' => 'required|string|min:6|confirmed',
+            'role' => 'required|string|in:user,admin',
+            'jk' => 'required|string|in:laki-laki,perempuan',
+        ]);
+
+        // Create new user
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'username' => $validatedData['username'],
+            'password' => Hash::make($validatedData['password']),
+            'role' => 'user',
+            'jk' => $validatedData['jk'],
+        ]);
+
+        // Optionally, create associated DataPasien record
+        $dataPasien = DataPasien::create([
+            'user_id' => $user->id,
+            'aktivitas_id' => $validatedData['aktivitas_id'],
+            'tb' => $validatedData['tb'],
+            'bb' => $validatedData['bb'],
+            'kolesterol' => $validatedData['username'],
+            'umur' => $user->id,
+        ]);
+
+        // Generate token
+        $token = $user->createToken('authToken')->plainTextToken;
+
+        // Return response with token and user data
+        return response()->json(['access_token' => $token, 'user_profile' => $user, 'data' => $dataPasien], 201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,11 @@
 <?php
 
 use App\Http\Controllers\API\AlternatifController;
+use App\Http\Controllers\API\AuthMobileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 
 Route::get('/food', [AlternatifController::class, 'index']);
+Route::post('/login', [AuthMobileController::class, 'login']);
+Route::post('/register', [AuthMobileController::class, 'login']);
